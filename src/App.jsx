0fc82f536962ae8eb6242555809eaa15e39a0297
JonahEG <jonah.edelmangold@gmail.com>
2025-04-01 16:57:33 -0500

    Initial Winner Function

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,25 @@ function Square ({ value, onSquareClick }){
     </button>
   );
 }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
@@ -46,7 +65,7 @@ export default function Board() {
 
 
   function handleClick(i) {
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice();
